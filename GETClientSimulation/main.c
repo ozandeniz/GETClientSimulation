b7a84158a7e37ac11595738479a7f8061bffa751
Ozan Deniz <denzozan@gmail.com>
2015-08-31 13:44:38 +0300

    Initial Commit

---
+++ GETClientSimulation/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GETClientSimulation
+//
+//  Created by Ozan Deniz on 31/08/15.
+//  Copyright (c) 2015 Ozan Deniz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
